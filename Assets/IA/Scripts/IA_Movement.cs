6c628fc98e2017a297081c5355d7f8c5a0eb0706
benjii66 <benjamin51.simon@gmail.com>
2021-01-08 00:56:10 +0100

    Add Players

--- Assets/IA/Scripts/IA_Movement.cs
+++ Assets/IA/Scripts/IA_Movement.cs
@@ -8,6 +8,7 @@ public class IA_Movement : MonoBehaviour
     public event Action OnPositionReached = null;
     [SerializeField]Vector3 targetPosition = Vector3.zero;
     [SerializeField,Range(0,10)]float atPosRange = 2;
+    [SerializeField, Range(0, 100)] float speed = 2;
 
     public bool IsAtPos => Vector3.Distance(targetPosition, transform.position) < atPosRange;
 
@@ -21,7 +22,7 @@ public class IA_Movement : MonoBehaviour
             OnPositionReached?.Invoke();
             return; 
         }
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition,speed * Time.deltaTime);
     }
 
     private void OnDrawGizmos()
--- Assets/Player/Scripts/P_Player.cs
+++ Assets/Player/Scripts/P_Player.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class P_Player : MonoBehaviour
 {
     [SerializeField] int health = 100;
+    [SerializeField] float speed = 1;
+    [SerializeField] float speedRotation = 1;
+    [SerializeField] Camera camera1 = null;
+    [SerializeField] Animator playerAnim = null;
+    [SerializeField] bool useClampValue = true;
+    [SerializeField] Transform target = null;
+    [SerializeField] float clampMaxValue = 60;
+    [SerializeField] float clampMinValue = 20;
 
-
+    Vector3 movement = Vector3.zero;
     public int Health => health;
-
+    float rotateY = 0, rotateX = 0;
     public void SetHealth(int _health) => health = _health;
 
     // Start is called before the first frame update
@@ -20,6 +28,23 @@ public class P_Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+ 
     }
+
+    void Move()
+	{
+ 
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        playerAnim.Play("Walk");
+        transform.LookAt(target);
+        movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        movement = movement * speed * Time.deltaTime;
+        transform.position += movement;
+    }
+
+    
+
+  
 }
