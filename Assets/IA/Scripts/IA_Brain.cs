73e870cb3ad27cb42c8ad4039d92927ea3161c8f
benjii66 <benjamin51.simon@gmail.com>
2021-01-07 14:24:15 +0100

    Create files and Animator Controller

---
+++ Assets/IA/Scripts/IA_Brain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Brain : MonoBehaviour
+{
+    [SerializeField] Animator fsm = null;
+
+    public bool IsValid => fsm;
+
+    private void Start()
+    {
+        InitFSM();
+    }
+
+    void InitFSM()
+    {
+        if (!IsValid) return;
+
+
+    }
+}
---
+++ Assets/Tools/HS_HierarchySetup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+public class HS_DefaultSetup
+{
+    [MenuItem("Project setup/Hierarchy")]
+    static void DefaultSetup()
+    {
+        SetObjectInHierarchy<Camera>("CAMERAS");
+        SetObjectInHierarchy<Light>("LIGHTS");
+        SetObjectInHierarchy<AudioSource>("AUDIO SOURCE");
+        CreateHierarchySection("PROPS");
+        CreateHierarchySection("LOGIC");
+        CreateHierarchySection("CHARACTERS");
+    }
+    //
+    static void SetObjectInHierarchy<T>(string _sectionName) where T : Component
+    {
+        int _sectionIndex = FindSection(_sectionName);
+        Type _type = typeof(T);
+        Object[] _m = GameObject.FindObjectsOfType(_type);
+        for (int i = 0; i < _m.Length; i++)
+        {
+            T _object = (T)Convert.ChangeType(_m[i], _type);
+            _object.transform.SetSiblingIndex(_sectionIndex);
+        }
+    }
+    static void CreateHierarchySection(string _sectionName)
+    {
+        GameObject _section = GameObject.Find($"------[{_sectionName}]------");
+        if (_section) return;
+        _section = new GameObject($"------[{_sectionName}]------");
+        _section = new GameObject($"------------------");
+    }
+    static int FindSection(string _sectionName)
+    {
+        GameObject _section = null;
+        if (!(_section = GameObject.Find($"------[{_sectionName}]------")))
+        {
+            _section = new GameObject($"------[{_sectionName}]------");
+            _section = new GameObject($"------------------");
+            return _section.transform.GetSiblingIndex() - 1;
+        }
+        return _section.transform.GetSiblingIndex() + 1;
+        //return _section;
+    }
+}
